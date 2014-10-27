1a309c054ada401a78fffa81fb58eadac2a0566e
Peter Olsen <pto@me.com>
2014-10-27 22:47:01 +0000

    changing from sleep to timer

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 sudo cp scan-journal.service /etc/systemd/system/
+sudo cp scan-journal.timer /etc/systemd/system/
 sudo cp scan-journal.sh /root
 sudo chmod +x /root/scan-journal.sh
+sudo systemctl daemon-reload
 sudo systemctl enable /etc/systemd/system/scan-journal.service
-sudo systemctl start scan-journal.service
+sudo systemctl enable /etc/systemd/system/scan-journal.timer
+sudo systemctl start scan-journal.timer
 
